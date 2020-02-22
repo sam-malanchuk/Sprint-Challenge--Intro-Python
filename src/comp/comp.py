f5399a740bbbe026934dfa4c10c64962ce9d0071
unknown <malanchuksam@gmail.com>
2020-02-22 13:26:46 -0500

    All PASSED in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,19 +24,19 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [h for h in humans if h.name.startswith('D')]
+a = [h.name for h in humans if h.name.startswith('D')]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = [h for h in humans if h.name.endswith('e')]
+b = [h.name for h in humans if h.name.endswith('e')]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [h for h in humans if (h.name[0] in ['C', 'D', 'E', 'F', 'G'])]
+c = [h.name for h in humans if (h.name[0] in ['C', 'D', 'E', 'F', 'G'])]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -54,7 +54,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(h.name, h.age) for h in humans if (h.age >= 27 and h.age <= 31)]
+f = [(h.name, h.age) for h in humans if (h.age >= 27 and h.age <= 32)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
