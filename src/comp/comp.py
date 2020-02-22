bcc03f98d65d0d5dd8cdc017ce14ae7679775f97
unknown <malanchuksam@gmail.com>
2020-02-22 13:09:54 -0500

    completed tasks 2 and 3 in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -30,13 +30,13 @@ print(a)
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [h for h in humans if h.name.endswith('e')]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [h for h in humans if (h.name[0] in ['C', 'D', 'E', 'F', 'G'])]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
